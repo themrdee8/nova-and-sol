4830c0c89d5c636a2b6fd54a15b1cfda074ec505
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2025-11-12 22:29:01 +0000

    3: Tailwind tweaked configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 // import Image from "next/image";
 
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        Nova and Sol
+        <Navbar />
         <Link href="/streetWear"><p>street wear</p></Link>
         <Link href="/theSolStrand"><p>the sol strand</p></Link>
         <Link href="/amari"><p>amari</p></Link>
---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div >
+        <div className='bg-blue-500'>
+            <p className='text-6xl'>hello</p>
+            <h1 className='text-8xl font-bold text-amber-300'>navbar</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
