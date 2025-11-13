2c12c5e4d9723f1234f0423a29f4f4b0f8478360
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2025-11-13 15:41:35 +0000

    5: Navbar animation and scroll behaviour

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,28 +5,28 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import { FiSearch } from "react-icons/fi";
 
 const Navbar = () => {
-  const [navBg, setNavBg] = useState("transparent")
+  const [scrollBg, setScrollBg] = useState(false);
 
   useEffect(() => {
     const handleNavBg = () => {
-      if (window.scrollY >= 90) setNavBg("white")
-      else setNavBg("transparent")
+      setScrollBg(window.scrollY > 50)
     }
 
     window.addEventListener("scroll", handleNavBg)
+    return () => window.removeEventListener("scroll", handleNavBg)
   }, [])
 
   return (
-    <div className={navBg ? 'flex justify-between p-2.5 items-center fixed w-full': ""}>
+    <div className={`flex justify-between transition-all duration-300 p-2.5 items-center fixed w-full ${scrollBg ? 'bg-white shadow-md': "bg-transparent"}`}>
         <div className=''>
-            <RxHamburgerMenu size={18} className='text-white' />
+            <RxHamburgerMenu size={20} className={`${scrollBg ? 'text-black': "text-white" }`} />
         </div>
-        <div>
-          <p className='font-Satisfy text-[20px] text-white'>Nova & Sol</p>
+        <div className='ml-2'>
+          <p className={`font-Satisfy transition-all duration-300 ${scrollBg ? 'text-black text-[18px]': "text-white text-[20px]" }`}>Nova & Sol</p>
         </div>
-        <div className='flex space-x-2'>
-          <FiSearch size={18} className='text-white' />
-          <MdOutlineShoppingCart size={18} className='text-white' />
+        <div className='flex space-x-4'>
+          <FiSearch size={20} className={`${scrollBg ? 'text-black': "text-white" }`} />
+          <MdOutlineShoppingCart size={20} className={`${scrollBg ? 'text-black': "text-white" }`} />
         </div>
     </div>
   )
