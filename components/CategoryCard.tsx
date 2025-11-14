246d0605a259e43b1233894b39d52a21ab444684
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2025-11-14 18:35:41 +0000

    7: Category card button functionality

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import cat from "@/public/images/cat.jpg"
-import Image from 'next/image'
-import Button from './Button'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import React from "react";
+import cat from "@/public/images/cat.jpg";
+import Image from "next/image";
+import Button from "./Button";
+import { useRouter } from "next/navigation";
 
 const CategoryCard = () => {
-    
-    const categories = [
-        {categoryName: "street wear", pageLink: "/streetWear", imageUrl: {cat}},
-        {categoryName: "the sol strand", pageLink: "/theSolStrand", imageUrl: {}},
-        {categoryName: "amari", pageLink: "/amari", imageUrl: {}},
-        {categoryName: "the charm bar", pageLink: "/theCharmBar", imageUrl: {}},
-        {categoryName: "the perfect find", pageLink: "/thePerfectFind", imageUrl: {}},
-    ]
+  const router = useRouter();
+  const categories = [
+    { categoryName: "street wear", pageLink: "/streetWear", imageUrl: cat },
+    {
+      categoryName: "the sol strand",
+      pageLink: "/theSolStrand",
+      imageUrl: cat,
+    },
+    { categoryName: "amari", pageLink: "/amari", imageUrl: cat },
+    {
+      categoryName: "the charm bar",
+      pageLink: "/theCharmBar",
+      imageUrl: cat,
+    },
+    {
+      categoryName: "the perfect find",
+      pageLink: "/thePerfectFind",
+      imageUrl: cat,
+    },
+  ];
 
-    // const handleClick = () => {
-
-    // }
+  const handleClick = (link: any) => {
+    router.push(link)
+  };
   return (
     <div>
-{categories.map((category) => (
-    <div key={category.categoryName} className='grid place-items-center'>
-        <Image src={cat} alt="cat" className='object-cover col-start-1 row-start-1 h-[350px]' />
-        <p className='col-start-1 row-start-1 uppercase text-white mt-[200px]'>{category.categoryName}</p>
-        <Button name="shop now" styles="col-start-1 row-start-1 mt-[280px]"/>
+      {categories.map((category) => (
+        <div key={category.pageLink} className="grid place-items-center">
+          <Image
+            src={category.imageUrl}
+            alt={category.categoryName}
+            className="object-cover col-start-1 row-start-1 h-[350px]"
+          />
+          <p className="col-start-1 row-start-1 uppercase text-white mt-[200px]">
+            {category.categoryName}
+          </p>
+          <Button
+            onclick={() => handleClick(category.pageLink)}
+            name="shop now"
+            styles="col-start-1 row-start-1 mt-[280px]"
+          />
+        </div>
+      ))}
     </div>
-))}
-        
-    </div>
-  )
-}
+  );
+};
 
-export default CategoryCard
+export default CategoryCard;
